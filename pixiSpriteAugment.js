d581224f629d3f281737c15fadd91924cfcbada6
hako <njucswuhao@163.com>
2016-07-14 17:36:52 +0800

    fix bug

--- pixiSpriteAugment.js
+++ pixiSpriteAugment.js
@@ -13,7 +13,7 @@ PIXI.Sprite.prototype.updateNodePosition = function(p) {
     this.position.y = p.y;
     if (this.ts) {
         this.ts.position.x = p.x;
-        this.ts.position.y = p.y + visualConfig.NODE_LABLE_OFFSET_Y;
+        this.ts.position.y = p.y + visualConfig.NODE_LABLE_OFFSET_Y*this.scale.y;
     }
     _.each(this.incoming, function(l) {
         l.setTo(p);
