c809bbd81a44fca0ee150951ef73a53e0ae2f8de
Yeling Shi <shyl@zqykj.com>
2017-06-13 16:09:05 +0800

    Preparing for multiple page and eslint

--- FPSCounter.js
+++ FPSCounter.js
@@ -1,26 +1,24 @@
-const FPSCounter = function() {
-    this.frameCount = 0;
-    this.lastUpdate = null;
-    this.value = "";
-    this.updateFrequency = 1000;
-};
+export default class FPSCounter {
+    constructor() {
+        this.frameCount = 0;
+        this.lastUpdate = null;
+        this.value = "";
+        this.updateFrequency = 1000;
+    }
 
-FPSCounter.prototype.nextFrame = function () {
-    var now = new Date();
+    nextFrame() {
+        var now = new Date();
 
-    this.frameCount++;
+        this.frameCount++;
 
-    if (this.lastUpdate !== null) {
-        if ((now - this.lastUpdate) > this.updateFrequency) {
-            this.value = ((this.frameCount * 1000) / (now - this.lastUpdate)).toFixed(1);
-            this.frameCount = 0;
+        if (this.lastUpdate !== null) {
+            if ((now - this.lastUpdate) > this.updateFrequency) {
+                this.value = ((this.frameCount * 1000) / (now - this.lastUpdate)).toFixed(1);
+                this.frameCount = 0;
+                this.lastUpdate = now;
+            }
+        } else {
             this.lastUpdate = now;
         }
-    } else {
-        this.lastUpdate = now;
     }
-};
-
-export {
-    FPSCounter
 }
